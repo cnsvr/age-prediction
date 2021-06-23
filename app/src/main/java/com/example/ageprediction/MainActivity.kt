ca4e4e8a7cc4e33f83ac60e423bf16461cc7f57b
sarismet <sarismet2825@gmail.com>
2021-06-23 18:46:29 +0300

    analyzing is started

--- app/src/main/java/com/example/ageprediction/MainActivity.kt
+++ app/src/main/java/com/example/ageprediction/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.ageprediction
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.app.Dialog
 import android.content.Intent
 import android.graphics.Bitmap
@@ -13,6 +12,7 @@ import android.media.ExifInterface
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Base64
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -22,7 +22,14 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+import com.github.kittinunf.fuel.Fuel
+import org.json.JSONObject
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.FileOutputStream
+import kotlin.concurrent.thread
+
+
 
 
 private const val FILE_NAME = "photo.jpg"
@@ -32,6 +39,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnTakePicture : Button
     private lateinit var imageView: ImageView
     var dialog: Dialog? = null
+    var address:String = "192.168.1.33"
+    var rotatedBitmap: Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -44,6 +53,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     @SuppressLint("QueryPermissionsNeeded")
     override fun onClick(view: View?) {
+        //startAnalyze()
+
         when(view?.id){
             R.id.btnTakePicture->{
                 val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
@@ -65,13 +76,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             result: ActivityResult ->
-        if (result.resultCode == Activity.RESULT_OK) {
+        if (result.resultCode == RESULT_OK) {
             val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
             val ei = ExifInterface(photoFile.absolutePath)
             val orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)
 
             // Rotation
-            var rotatedBitmap: Bitmap? = null
+
             rotatedBitmap = when (orientation) {
                 ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(takenImage, 90F)
                 ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(takenImage, 180F)
@@ -81,20 +92,62 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             imageView.setImageBitmap(rotatedBitmap)
+             try {
+
+                //Convert bitmap to byte array
+                val bos = ByteArrayOutputStream()
+                 rotatedBitmap!!.compress(Bitmap.CompressFormat.PNG, 90, bos) // YOU can also save it in JPEG
+                val bitmapdata = bos.toByteArray()
+
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
             val analysebutton = findViewById(R.id.analysebutton) as Button
             analysebutton.setVisibility(View.VISIBLE);
             analysebutton.setOnClickListener {
                 Toast.makeText(this@MainActivity, "Analyzing is started.", Toast.LENGTH_SHORT).show()
-                this.startAnalyze()
+                var path = photoFile.absolutePath + File.separator + "theimage"
+
+                startAnalyze()
             }
         }
     }
+
+
     fun startAnalyze() {
-        System.out.println("We found the result")
-        this.showResult(12)
+        val stream = ByteArrayOutputStream()
+        rotatedBitmap?.compress(Bitmap.CompressFormat.PNG, 99, stream)
+        val image = stream.toByteArray()
+        System.out.println("image length : "+image.size)
+        val encodedImage = String(image)
+        val map = HashMap<String, String>()
+
+        map.put("image_byte_array", encodedImage)
+        val msg:String = JSONObject(map as Map<*, *>).toString()
+        var result:String = ""
+        val job = thread {
+
+            Fuel.post("http://3.17.70.149:5000/send_image")
+                .body(
+                    msg
+                ).response { request, response, result ->
+                    println(request)
+                    println(response.body())
+
+                    val (bytes, error) = result
+                    if (bytes != null) {
+                        println("[response bytes] ${String(bytes)}")
+                        showResult(String(bytes))
+                    }
+                }
+        }
+
+
     }
 
-    fun showResult(age: Int){
+
+    fun showResult(age: String){
         dialog!!.setContentView(R.layout.activity_result)
         dialog!!.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
@@ -102,31 +155,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val girlImage: ImageView = this.dialog!!.findViewById<ImageView>(R.id.girlImage)
         val ageText: TextView = this.dialog!!.findViewById<TextView>(R.id.ageText)
 
-        if (age <= 2){
+        if (age == "1-2"){
             boyImage.setImageResource(R.drawable.firstboy);
             girlImage.setImageResource(R.drawable.firstgirl);
             ageText.text = "Your age grup is : 1-2 "
-        }else if (age <= 9){
+        }else if (age ==  "3-9"){
             boyImage.setImageResource(R.drawable.secondboy);
             girlImage.setImageResource(R.drawable.secondgirl);
             ageText.text = "Your age grup is : 3-9 "
         }
-        else if (age <= 20){
+        else if (age == "10-20"){
             boyImage.setImageResource(R.drawable.thirdboy);
             girlImage.setImageResource(R.drawable.thirdgirl);
             ageText.text = "Your age grup is : 10-20 "
         }
-        else if (age <= 27){
+        else if (age == "21-27"){
             boyImage.setImageResource(R.drawable.forthboy);
             girlImage.setImageResource(R.drawable.forthgirl);
             ageText.text = "Your age grup is : 21-27 "
         }
-        else if (age <= 45){
+        else if (age == "28-45"){
             boyImage.setImageResource(R.drawable.fifthboy);
             girlImage.setImageResource(R.drawable.fifthgirl);
             ageText.text = "Your age grup is : 28-45 "
         }
-        else if (age <= 65){
+        else if (age == "46-65"){
             boyImage.setImageResource(R.drawable.sixthboy);
             girlImage.setImageResource(R.drawable.sixthgirl);
             ageText.text = "Your age grup is : 46-65 "
@@ -135,7 +188,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             girlImage.setImageResource(R.drawable.lastwomen);
             ageText.text = "Your age grup is : +65 "
         }
-        System.out.println("Win Alert is called")
+
 
 
         val btnOk:Button = this.dialog!!.findViewById<Button>(R.id.btnOK)
